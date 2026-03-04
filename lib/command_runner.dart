17cebe7d4f1a02ee93cab906f350ffba2190c725
mono <mono0926@gmail.com>
2026-03-04 17:40:14 +0900

    feat: improve CLI foundation with cli_completion and error handling

--- lib/command_runner.dart
+++ lib/command_runner.dart
@@ -1,8 +1,10 @@
 import 'package:args/command_runner.dart';
+import 'package:cli_completion/cli_completion.dart';
+import 'package:script/exceptions.dart';
 import 'package:script/logger.dart';
 import 'package:script/src/commands/setup_skills.dart';
 
-class ScriptCommandRunner extends CommandRunner<int> {
+class ScriptCommandRunner extends CompletionCommandRunner<int> {
   ScriptCommandRunner()
     : super(
         'script',
@@ -15,21 +17,25 @@ class ScriptCommandRunner extends CommandRunner<int> {
   Future<int?> run(Iterable<String> args) async {
     try {
       return await super.run(args);
+    } on FormatException catch (e) {
+      logger
+        ..err(e.message)
+        ..info('')
+        ..info(usage);
+      return 64; // usage error
     } on UsageException catch (e) {
       logger
         ..err(e.message)
         ..info('')
         ..info(e.usage);
       return 64; // EX_USAGE
+      // 現在は投げられていないが、将来の拡張を見据えてドメイン固有の例外ハンドリングを準備
+    } on AppException catch (e) {
+      logger.err(e.message);
+      return 1;
     } on Exception catch (e, stackTrace) {
       logger
-        ..err(e.toString())
-        ..err(stackTrace.toString());
-      return 1;
-      // ignore: avoid_catching_errors
-    } on Error catch (e, stackTrace) {
-      logger
-        ..err(e.toString())
+        ..err('An unexpected error occurred: $e')
         ..err(stackTrace.toString());
       return 1;
     }
